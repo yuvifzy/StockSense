f77a7fd86962404b8028225a1635de97428300b0
yuvifzy <yuvrajmohana07@gmail.com>
2026-06-13 12:51:28 +0530

    changes to mai.py

--- stocksense/app/main.py
+++ stocksense/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "https://stocksense-app.up.railway.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
